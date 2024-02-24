011e8101977b563d1ef0ce18cbc473ede00bfe9d
iceninja3 <98727663+iceninja3@users.noreply.github.com>
2024-02-24 00:33:08 -0800

    Create Retract.ino

---
+++ float/Retract.ino
@@ -0,0 +1,21 @@
+//Code for only retract to the max (i.e. sinking mode)
+
+// constants won't change
+const int IN1_PIN = 6; // the Arduino pin connected to the IN1 pin L298N
+const int IN2_PIN = 5; // the Arduino pin connected to the IN2 pin L298N
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pins as outputs.
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  //retract the actuator
+  digitalWrite(IN1_PIN, HIGH);
+  digitalWrite(IN2_PIN, LOW);
+
+  delay(35000); // actuator will stop extending automatically when reaching the limit
+}
