7fadd2dd3878c70b2140fb1b67ee915448316fc2
superfluid-helium <144962990+superfluid-helium@users.noreply.github.com>
2024-05-03 16:44:22 -0700

    cause running would always be true

--- ImgProc/picthingy.py
+++ ImgProc/picthingy.py
@@ -2,15 +2,14 @@ import cv2
 from time import sleep
   
 vid = cv2.VideoCapture(0) 
-i = 1
-running = True  
+i = 1 
 while(True): 
       
     ret, frame = vid.read() 
   
     cv2.imshow('frame', frame) 
     
-    while running:
+    while i<30:
         cv2.imwrite(r'C:\Users\dogei\python\MATE_ROV\CAMERA\mee' + str(i) + '.jpg', frame)
         print('taken')
         print(i)
